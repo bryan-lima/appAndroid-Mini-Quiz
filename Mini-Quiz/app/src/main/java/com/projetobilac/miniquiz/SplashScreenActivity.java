6a99e7af4605eca6371d8e25fd7e8bb9ef5ff69e
Bryan <bryanlima200@outlook.com>
2020-03-12 11:17:11 -0300

    Adicionado tempo de transição para SplashScreenActivity O app iniciará com a tela Splash Screen, e após 2000 milisegundos irá para a tela Main, onde o usuário poderá interagir

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SplashScreenActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.projetobilac.miniquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                chamarMain();
+            }
+        }, 2000);
+    }
+
+    private void chamarMain(){
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
