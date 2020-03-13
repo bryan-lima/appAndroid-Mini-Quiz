d0073e21d0cc6dcf9b010f30fda5784a9593e47d
Bryan <bryanlima200@outlook.com>
2020-03-13 01:19:52 -0300

    Em MainActivity.java, configurado onBackPressed e botões Start e Sobre

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/MainActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/MainActivity.java
@@ -1,14 +1,68 @@
 package com.projetobilac.miniquiz;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context = MainActivity.this;
+    private Button btnStart, btnSobre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.mai_btn_start);
+        btnSobre = findViewById(R.id.mai_btn_sobre);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SobreActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        alertDialogOnBackPressed("Sair?", "Deseja sair do app?");
+    }
+
+    private void alertDialogOnBackPressed (String strTitle, String strMsg) {
+        new AlertDialog.Builder(context, R.style.Theme_AppCompat_Dialog_Alert)
+                .setTitle(strTitle)
+                .setMessage(strMsg)
+                .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    // m.alertToast(context, "Nenhuma ação foi realizada.");
+                    }
+                })
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finishAffinity();
+                    }
+                }).show();
     }
 }
