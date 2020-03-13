d11ca3f23eb14cff30f24bd25cc61d25de62ece3
Bryan <bryanlima200@outlook.com>
2020-03-13 02:34:32 -0300

    Configurado backend da activity RespostaErrada Configurado botão voltar e onBackPressed, em que ambos retornam para a activity Quiz

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
@@ -2,13 +2,38 @@ package com.projetobilac.miniquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RespostaErradaActivity extends AppCompatActivity {
+
+    private Context context = RespostaErradaActivity.this;
+    private Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta_errada);
+
+        btnVoltar = findViewById(R.id.res_err_btn_voltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, QuizActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(context, QuizActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
