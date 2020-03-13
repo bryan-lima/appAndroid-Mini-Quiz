cb1aa64474beaa98ca15de35dc63de0ed0b9ad95
Bryan <bryanlima200@outlook.com>
2020-03-13 00:51:42 -0300

    Acrescentado informações na acitivity_sobre.xml

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SobreActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SobreActivity.java
@@ -3,12 +3,19 @@ package com.projetobilac.miniquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class SobreActivity extends AppCompatActivity {
+
+    private TextView linkRepositorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+
+        linkRepositorio = findViewById(R.id.sob_txt_link_repositorio);
+        linkRepositorio.setText(Html.fromHtml("<a href=\'https://github.com/bryan-lima/appAndroid-Mini-Quiz\'>github.com/bryan-lima/appAndroid-Mini-Quiz</a>"));
     }
 }
