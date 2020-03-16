a6f168a817a65e6d9d194e58f129a284e9034856
Bryan <bryanlima200@outlook.com>
2020-03-16 18:38:32 -0300

    Em SobreActivity.java, configurado navbar programaticamente Programado título (Sobre) Inserido ícone para voltar à tela anterior (setDisplayHomeAsUpEnabled) Habilitado click do ícone (setHomeButtonEnabled) Programado ícone para retornar para MainActivity.class

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SobreActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/SobreActivity.java
@@ -1,5 +1,6 @@
 package com.projetobilac.miniquiz;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,6 +24,8 @@ public class SobreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
 
+        configuraNavBar();
+
         btnVoltar = findViewById(R.id.sob_btn_voltar);
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +40,33 @@ public class SobreActivity extends AppCompatActivity {
         linkRepositorio = findViewById(R.id.sob_txt_link_repositorio);
         linkRepositorio.setText(Html.fromHtml("<a href=\'https://github.com/bryan-lima/appAndroid-Mini-Quiz\'>github.com/bryan-lima/appAndroid-Mini-Quiz</a>"));
         linkRepositorio.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void configuraNavBar() {
+        setTitle("Sobre");
+        ActionBar actionBar = getSupportActionBar(); // instancia objt da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // habilita click
+    }
+
+    // Para inserir a ação e selecionar para qual página voltar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(context, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
