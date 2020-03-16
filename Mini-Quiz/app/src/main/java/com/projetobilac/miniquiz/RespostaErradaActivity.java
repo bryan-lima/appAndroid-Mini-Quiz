792f85d67429a0f6494657603fc162cd7eba789d
Bryan <bryanlima200@outlook.com>
2020-03-16 18:44:10 -0300

    Em RespostaErradaActivity.java, configurado navbar programaticamente Inserido ícone para voltar à tela anterior (setDisplayHomeAsUpEnabled) Habilitado click do ícone (setHomeButtonEnabled) Programado ícone para retornar para QuizActivity.class

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
@@ -1,10 +1,12 @@
 package com.projetobilac.miniquiz;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,6 +24,8 @@ public class RespostaErradaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta_errada);
 
+        configuraNavBar();
+
         gifRespErrada = findViewById(R.id.res_err_img_gif);
 
         Glide.with(this).load(R.drawable.gif_resp_errada).into(gifRespErrada);
@@ -36,6 +40,27 @@ public class RespostaErradaActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void configuraNavBar() {
+        setTitle("Errou!");
+        ActionBar actionBar = getSupportActionBar(); // instancia objt da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // habilita click
+    }
+
+    // Para inserir a ação e selecionar para qual página voltar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, QuizActivity.class);
+                startActivity(intent);
+                finish();
+            default:
+                break;
+        }
+        return true;
     }
 
     @Override
