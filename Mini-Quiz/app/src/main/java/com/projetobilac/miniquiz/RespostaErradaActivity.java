69832ffa106007205cec5e288a59e4296e7f79c3
Bryan <bryanlima200@outlook.com>
2020-03-16 17:26:22 -0300

    Em RespostaErradaActivity.java, adicionado programaticamente o gif_resp_errada na activity_resposta_errada.xml Utilizando a dependência Glide, adicionado anteriormente ao projeto Não definido limite para execução do gif, o padrão é executar em loop

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaErradaActivity.java
@@ -7,16 +7,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class RespostaErradaActivity extends AppCompatActivity {
 
     private Context context = RespostaErradaActivity.this;
     private Button btnVoltar;
+    private ImageView gifRespErrada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta_errada);
+
+        gifRespErrada = findViewById(R.id.res_err_img_gif);
+
+        Glide.with(this).load(R.drawable.gif_resp_errada).into(gifRespErrada);
 
         btnVoltar = findViewById(R.id.res_err_btn_tentar_novamente);
         btnVoltar.setOnClickListener(new View.OnClickListener() {
