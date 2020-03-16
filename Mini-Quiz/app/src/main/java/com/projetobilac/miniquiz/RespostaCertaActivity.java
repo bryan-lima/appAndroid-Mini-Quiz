e592fa14da8892cea1a6a37564a83fd1689b0f76
Bryan <bryanlima200@outlook.com>
2020-03-16 15:50:34 -0300

    Em RespostaCertaActivity.java, adicionado programaticamente o gif_resp_certa na activity_resposta_certa.xml Utilizando a dependência Glide, adcionando anteriormente ao projeto Também definido para que o gif execute apenas 1 vez (setLoopCount(1))

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaCertaActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/RespostaCertaActivity.java
@@ -1,22 +1,57 @@
 package com.projetobilac.miniquiz;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 public class RespostaCertaActivity extends AppCompatActivity {
 
     private Context context = RespostaCertaActivity.this;
     private Button btnRetornarMain;
+    private ImageView gifRespCerta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta_certa);
+
+        gifRespCerta = findViewById(R.id.res_cer_img_gif);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.gif_resp_certa)
+                .listener(new RequestListener<GifDrawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                        resource.setLoopCount(1);
+                        resource.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                            @Override
+                            public void onAnimationEnd(Drawable drawable) {
+                                //do whatever after specified number of loops complete
+                            }
+                        });
+                        return false;
+                    }}).into(gifRespCerta);
 
         btnRetornarMain = findViewById(R.id.res_cer_btn_voltar_home);
         btnRetornarMain.setOnClickListener(new View.OnClickListener() {
