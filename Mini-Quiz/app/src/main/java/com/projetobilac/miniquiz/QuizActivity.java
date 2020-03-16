de2652a002c331a8b140fa2e412d0ac50503e592
Bryan <bryanlima200@outlook.com>
2020-03-16 18:39:39 -0300

    Em QuizActivity.java, configurado navbar programaticamente Programado título (Mini Quiz) Inserido ícone para voltar à tela anterior (setDisplayHomeAsUpEnabled) Habilitado click do ícone (setHomeButtonEnabled) Programado ícone para retornar para MainActivity.class

--- Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/QuizActivity.java
+++ Mini-Quiz/app/src/main/java/com/projetobilac/miniquiz/QuizActivity.java
@@ -1,10 +1,12 @@
 package com.projetobilac.miniquiz;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,8 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        configuraNavBar();
 
         btnOpcao1 = findViewById(R.id.qui_btn_opcao_1);
         btnOpcao2 = findViewById(R.id.qui_btn_opcao_2);
@@ -59,6 +63,27 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void configuraNavBar() {
+        setTitle("Mini Quiz");
+        ActionBar actionBar = getSupportActionBar(); // instancia objt da BAR
+        actionBar.setDisplayHomeAsUpEnabled(true); // exibe o ícone
+        actionBar.setHomeButtonEnabled(true); // habilita click
+    }
+
+    // Para inserir a ação e selecionar para qual página voltar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+            default:
+                break;
+        }
+        return true;
     }
 
     @Override
